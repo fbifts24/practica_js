2d9351ad400a2a2d4abf954d618fd0de54e1454d
Federico Balbuena <federico.balbuena@ifts24.edu.ar>
2026-04-22 20:48:02 -0300

    Subo practica bloque try catch

---
+++ 04_tryCatch.js
@@ -0,0 +1,36 @@
+// MANEJO DE ERRORES
+
+// console.log(undefinedVariable)
+
+try {
+    console.log('Intento ejecutar el codigo')
+
+    console.log('Sale con fritas')
+
+} catch (error) {
+    console.log('Algo salio mal, ', error)
+}
+
+console.log('***************************************')
+
+try {
+    console.log('Segundo intento de ejecutar el codigo')
+    throw 'hay un error en el codigo'
+    console.log('Sale con fritas')
+} catch (error) {
+    console.log('Algo salio mal,', error)
+}
+
+console.log('***************************************')
+
+try {
+    console.log('Tercer intento de ejecutar el codigo')
+    throw 'hay un error en el codigo'
+    console.log('Sale con fritas')
+} catch (error) {
+    console.log('Algo salio mal,', error)
+} finally {
+    console.log('El bloque try catch termino')
+}
+
+console.log('***************************************')
